57b5d5148f76d851229dcec7bd2a8bcdddabc4c7
Dorian Depriester <dorian@crans.org>
2019-03-28 10:01:17 +0100

    Remove generate_2d and allow mutliple generation
    -Remove unnecessary function generate_2d()
    -change function name to lowercase (w/ rt PEP 8)
    -allow multiple generation of Euler angles at the same time

--- random_euler.py
+++ random_euler.py
@@ -1,20 +1,13 @@
-"""Random rotation matrix generators."""
-
 import numpy as np
-
 
 def generate(dim):
     """Generate a random rotation matrix.
-
     Args:
         dim (int): The dimension of the matrix.
-
     Returns:
         np.matrix: A rotation matrix.
-
     Raises:
         ValueError: If `dim` is not 2 or 3.
-
     """
     if dim == 2:
         return generate_2d()
@@ -24,26 +17,10 @@ def generate(dim):
         raise ValueError('Dimension {} is not supported. Use 2 or 3 instead.'
                          .format(dim))
 
-
-def generate_2d():
-    """Generate a 2D random rotation matrix.
-
-    Returns:
-        np.matrix: A 2D rotation matrix.
-
-    """
-    x = np.random.random()
-    M = np.matrix([[np.cos(2 * np.pi * x), -np.sin(2 * np.pi * x)],
-                   [np.sin(2 * np.pi * x), np.cos(2 * np.pi * x)]])
-    return M
-
-
 def generate_3d():
     """Generate a 3D random rotation matrix.
-
     Returns:
         np.matrix: A 3D rotation matrix.
-
     """
     x1, x2, x3 = np.random.rand(3)
     R = np.matrix([[np.cos(2 * np.pi * x1), np.sin(2 * np.pi * x1), 0],
@@ -54,22 +31,26 @@ def generate_3d():
                    [np.sqrt(1 - x3)]])
     H = np.eye(3) - 2 * v * v.T
     M = -H * R
+
     return M
 
-def generate_Euler():
-	M = generate_3d()
-	Phi = np.arccos(M[2,2])
-	if Phi == 0.0:
-		phi1 = np.arctan2(-M[1,0], M[0,0])
-		phi2 = 0	
-	elif Phi == np.pi:
-		phi1 = np.arctan2(M[1,0], M[0,0])
-		phi2 = 0
+def generate_euler(n=1):
+	if n==1:
+		M = generate_3d()
+		Phi = np.arccos(M[2,2])
+		if Phi == 0.0:
+			phi1 = np.arctan2(-M[1,0], M[0,0])
+			phi2 = 0	
+		elif Phi == np.pi:
+			phi1 = np.arctan2(M[1,0], M[0,0])
+			phi2 = 0
+		else:
+			phi1 = np.arctan2(M[2,0], -M[2,1])
+			phi2 = np.arctan2(M[0,2], M[1,2])
+		if phi1 < 0:
+			phi1 += 2*np.pi
+		if phi2 < 0:
+			phi2 += 2*np.pi
+		return phi1,Phi,phi2
 	else:
-		phi1 = np.arctan2(M[2,0], -M[2,1])
-		phi2 = np.arctan2(M[0,2], M[1,2])
-	if phi1 < 0:
-		phi1 += 2*np.pi
-	if phi2 < 0:
-		phi2 += 2*np.pi
-	return phi1,Phi,phi2
+		return [generate_Euler() for i in range(0,n)]
