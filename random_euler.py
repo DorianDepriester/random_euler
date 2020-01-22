b0fe7d11f8a4a96e211f6cb1b1b41159dfd897a5
Dorian Depriester <dorian@crans.org>
2020-01-22 11:14:11 +0100

    use scipy.stats.special_ortho_group for generating a random SO(3) matrix rename generate_euler() into random_euler()

--- random_euler.py
+++ random_euler.py
@@ -1,39 +1,26 @@
 import numpy as np
+from scipy import stats
 
-def generate_3d():
-    """Generate a 3D random rotation matrix.
-    Returns:
-        np.matrix: A 3D rotation matrix.
-    """
-    x1, x2, x3 = np.random.rand(3)
-    R = np.matrix([[np.cos(2 * np.pi * x1), np.sin(2 * np.pi * x1), 0],
-                   [-np.sin(2 * np.pi * x1), np.cos(2 * np.pi * x1), 0],
-                   [0, 0, 1]])
-    v = np.matrix([[np.cos(2 * np.pi * x2) * np.sqrt(x3)],
-                   [np.sin(2 * np.pi * x2) * np.sqrt(x3)],
-                   [np.sqrt(1 - x3)]])
-    H = np.eye(3) - 2 * v * v.T
-    M = -H * R
-
-    return M
-
-def generate_euler(n=1):
-	if n==1:
-		M = generate_3d()
+def mat2euler(M):
 		Phi = np.arccos(M[2,2])
 		if Phi == 0.0:
 			phi1 = np.arctan2(-M[1,0], M[0,0])
-			phi2 = 0	
+			phi2 = 0.0	
 		elif Phi == np.pi:
 			phi1 = np.arctan2(M[1,0], M[0,0])
-			phi2 = 0
+			phi2 = 0.0
 		else:
 			phi1 = np.arctan2(M[2,0], -M[2,1])
 			phi2 = np.arctan2(M[0,2], M[1,2])
-		if phi1 < 0:
+		if phi1 < 0.0:
 			phi1 += 2*np.pi
-		if phi2 < 0:
+		if phi2 < 0.0:
 			phi2 += 2*np.pi
-		return phi1,Phi,phi2
-	else:
-		return [generate_euler() for i in range(0,n)]
+		return phi1,Phi,phi2    
+
+def random_euler(n=1,**kwargs):
+    M = stats.special_ortho_group.rvs(dim=3,size=n,**kwargs)
+    if n==1:
+        return mat2euler(M)
+    else:
+        return [mat2euler(M[i,:,:]) for i in range(0,n)]
